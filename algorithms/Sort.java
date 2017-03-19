5ea4476e1465d1a145c1e1f02a864e4c88600a7c
metaphallus <weatherbreak@wine.ocn.ne.jp>
2017-03-19 01:26:24 -0700

    Update Sort.java

--- algorithms/Sort.java
+++ algorithms/Sort.java
@@ -3,8 +3,9 @@ public class Sort {
     int arr[] = {8, 3, 1, 2, 7, 5, 6, 4};
     //bubbleSort(arr);
     //insertionSort(arr);
-    selectionSort(arr);
+    //selectionSort(arr);
     //shellSort(arr);
+    quickSort(arr, 0, arr.length-1);
     for (int v: arr)
       System.out.println(v);
   }
@@ -37,6 +38,29 @@ public class Sort {
           if (arr[j-g] < arr[j]) break;
           swap(arr, j, j-g);
         }
+  }
+  
+  public static void quickSort(int arr[], int left, int right) {
+    if (left >= right) return;
+    
+    int index = partition(arr, left, right);
+    quickSort(arr, left, index - 1);
+    quickSort(arr, index, right);
+  }
+  
+  public static int partition(int arr[], int left, int right) {
+    int pivot = arr[(left + right) / 2];
+    
+    while (left <= right) {
+      while (arr[left] < pivot) left++;
+      while (arr[right] > pivot) right--;
+      if (left <= right) {
+        swap(arr, left, right);
+        left++;
+        right--;
+      }
+    }
+    return left;
   }
   
   public static void swap(int[] arr, int a, int b) {
